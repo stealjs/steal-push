5a85bde2f63d05127f91e8cadb60fc954d5b7113
Matthew Phillips <matthew@matthewphillips.info>
2017-07-26 10:46:13 -0400

    Only gzip javascript if accept-encoding header
    Is provided and gzip is an option. Closes #3

--- test/test.js
+++ test/test.js
@@ -16,6 +16,9 @@ describe("Basics", function(){
 
 	it("works", function(){
 		var req = through(() => {});
+		req.headers = {
+			"accept-encoding": "gzip, deflate, br"	
+		};
 		var res = through(() => {});
 
 		var pushes = [];
@@ -29,4 +32,32 @@ describe("Basics", function(){
 
 		assert.equal(pushes.length, 2, "There are pushes for style and script");
 	});
+
+	it("does not gzip content if header doesn't specify", function(done){
+		var req = through(() => {});
+		req.headers = {
+			"accept-encoding": "deflate, br"	
+		};
+		var res = through(() => {});
+
+		var pushes = [];
+		res.push = function(pth, options){
+			var p = through((buf, enc, cb) => {
+				// If a javascript result
+				if(/\.js/.test(pth)) {
+					var js = buf.toString();
+					assert.ok(/console\.log/.test(js), "This was not gzipped");
+					done();
+				}
+				cb();
+			});
+			pushes.push(p);
+			return p;
+		};
+
+		push(req, res);
+
+		assert.equal(pushes.length, 2, "There are pushes for style and script");
+	});
+
 });
