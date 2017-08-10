376d0ec631c0162bae3a8b567ddcfbb1d48e9e12
Matthew Phillips <matthew@matthewphillips.info>
2017-08-10 12:42:22 -0400

    Add support for <link rel=preload>
    This adds support for <link rel=preload> when the server is an HTTP1
    server. This is through a header like:
    
    ```
    Link: </dist/bundles/index.js>; rel=preload; as=script;
    ```
    
    Closes #4

---
+++ test/test_preload.js
@@ -0,0 +1,31 @@
+const assert = require("assert");
+const merge = require("merge-stream");
+const through = require("through2");
+const sp = require("../lib/index.js");
+const Writable = require("stream").Writable;
+
+describe("Preload", function(){
+	var stealPush = sp.create({
+		manifest: __dirname + "/tests/basics/bundles.json",
+		cwd: __dirname + "/tests/basics"
+	});
+
+	var push = stealPush("index");
+
+	it("works", function(){
+		var req = through(() => {});
+		req.headers = {
+			"accept-encoding": "gzip, deflate, br"	
+		};
+		var res = through(() => {});
+		res.push = undefined;
+
+		var pushes = [];
+		res.setHeader = (name, val) => {
+			pushes.push(val);
+		};
+
+		push(req, res);
+		assert.equal(pushes.length, 2, "There are preloads for style and script");
+	});
+});
---
+++ test/test_push.js
@@ -0,0 +1,61 @@
+const assert = require("assert");
+const merge = require("merge-stream");
+const through = require("through2");
+const sp = require("../lib/index.js");
+const Writable = require("stream").Writable;
+
+describe("PUSH", function(){
+	var stealPush = sp.create({
+		manifest: __dirname + "/tests/basics/bundles.json",
+		cwd: __dirname + "/tests/basics"
+	});
+
+	var push = stealPush("index");
+
+	it("works", function(){
+		var req = through(() => {});
+		req.headers = {
+			"accept-encoding": "gzip, deflate, br"	
+		};
+		var res = through(() => {});
+
+		var pushes = [];
+		res.push = function(pth, options){
+			var p = through((a, b, cb) => cb());
+			pushes.push(p);
+			return p;
+		};
+
+		push(req, res);
+
+		assert.equal(pushes.length, 2, "There are pushes for style and script");
+	});
+
+	it("does not gzip content if header doesn't specify", function(done){
+		var req = through(() => {});
+		req.headers = {
+			"accept-encoding": "deflate, br"	
+		};
+		var res = through(() => {});
+
+		var pushes = [];
+		res.push = function(pth, options){
+			var p = through((buf, enc, cb) => {
+				// If a javascript result
+				if(/\.js/.test(pth)) {
+					var js = buf.toString();
+					assert.ok(/console\.log/.test(js), "This was not gzipped");
+					done();
+				}
+				cb();
+			});
+			pushes.push(p);
+			return p;
+		};
+
+		push(req, res);
+
+		assert.equal(pushes.length, 2, "There are pushes for style and script");
+	});
+
+});
